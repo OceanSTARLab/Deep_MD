dfb980222addef01813fecbcafec020b4c10e0a7
OceanSTAR Lab <116273999+OceanSTARLab@users.noreply.github.com>
2023-12-09 17:20:00 +0800

    Add files via upload

---
+++ DeepMD-TL/ALL-KRAKEN/ALL_Pekeris/command_draft_1_Pekeris.m
@@ -0,0 +1,13 @@
+clc;clear;close;
+global  units
+units  = 'km';
+
+kraken Pekeris
+subplot(131);  plotshd Pekeris.shd
+
+krakenc Pekeris
+subplot(132);  plotshd Pekeris.shd
+
+scooter PekerisS
+subplot(133);  plotshd( 'PekerisS.shd' )
+
---
+++ DeepMD-TL/ALL-KRAKEN/KRAKEN/krakensd2018.m
@@ -0,0 +1,6 @@
+clc;clear;close;
+global  units
+units  = 'km';
+
+kraken Pekeris
+plotshd Pekeris.shd
